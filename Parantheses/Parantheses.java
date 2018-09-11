9b492d99acca0fe1520e74be03a5e0412c74a293
Saena <toastyhatesthesea@gmail.com>
2018-09-10 19:42:09 -0700

    Finished validParanHelper

--- Parantheses/Parantheses.java
+++ Parantheses/Parantheses.java
@@ -1,20 +1,48 @@
 package Parantheses;
 
+import java.util.Stack;
+
 public class Parantheses
 {
 
 
     public boolean isValid(String s)
     {
+        Stack<String> paranStack = new Stack<>();
         for (int i = 0; i < s.length(); i++)
         {
             String aParan = s.substring(i, i + 1);
+
+            if (aParan.equals("[") || aParan.equals("{") || aParan.equals("("))
+            {
+                paranStack.push(aParan);
+            }
         }
     }
 
-    public boolean validParanHelper(String aString)
+    public boolean validParanHelper(String rightParan, String leftParan)
     {
 
+        switch (rightParan)
+        {
+            case "}":
+                if (leftParan.equals("{"))
+                {
+                    return true;
+                }
+            case "]":
+                if (leftParan.equals("["))
+                {
+                    return true;
+                }
+            case ")":
+                if (leftParan.equals("("))
+                {
+                    return true;
+                }
+            default:
+                return false;
+        }
     }
 
 }
