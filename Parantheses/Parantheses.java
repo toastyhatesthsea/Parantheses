b465f014d41d92b3819613d4412f0abe0058a7a0
Saena <toastyhatesthesea@gmail.com>
2018-09-10 19:52:14 -0700

    Added Logic to isValid function

--- Parantheses/Parantheses.java
+++ Parantheses/Parantheses.java
@@ -9,15 +9,30 @@ public class Parantheses
     public boolean isValid(String s)
     {
         Stack<String> paranStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++)
+        boolean isValidParan = true;
+        for (int i = 0; i < s.length() && isValidParan; i++)
         {
             String aParan = s.substring(i, i + 1);
 
             if (aParan.equals("[") || aParan.equals("{") || aParan.equals("("))
             {
                 paranStack.push(aParan);
+            } else if (aParan.equals("]") || aParan.equals("}") || aParan.equals(")"))
+            {
+                if (!paranStack.empty())
+                {
+                    String leftParan = paranStack.pop();
+                    isValidParan = validParanHelper(aParan, leftParan);
+                } else
+                {
+                    isValidParan = false;
+                }
+            } else
+            {
+                isValidParan = false;
             }
         }
+        return isValidParan;
     }
 
     public boolean validParanHelper(String rightParan, String leftParan)
