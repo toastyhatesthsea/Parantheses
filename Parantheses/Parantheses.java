9885dc8079c0f148d5974b9293968578d8a2984a
Saena <toastyhatesthesea@gmail.com>
2018-09-10 19:33:11 -0700

    Creating Helper function and forming isValid function

---
+++ Parantheses/Parantheses.java
@@ -0,0 +1,20 @@
+package Parantheses;
+
+public class Parantheses
+{
+
+
+    public boolean isValid(String s)
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            String aParan = s.substring(i, i + 1);
+        }
+    }
+
+    public boolean validParanHelper(String aString)
+    {
+
+    }
+
+}
